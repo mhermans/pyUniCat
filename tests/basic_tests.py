6ca533ca7847a724eed7286dfc028157bbce61ae
Maarten Hermans <maarten.hermans@gmail.com>
2011-09-17 17:56:59 +0200

    start nose-tests

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -1,6 +1,13 @@
+import sys
+sys.path.append('../')
 from api import Query
 
 def test_cql_construction():
+    p = Query('lifecourse')
     q = Query()
     q.cqlp['term'] = 'lifecourse'
-    #assert q.cql = 'srw.Ser
+    assert p.cql == q.cql ==  'srw.ServerChoice = "lifecourse"'
+
+    p.cqlp['year'] = 1990
+    q.cqlp['year'] = '1990'
+    assert p.cql == q.cql == 'srw.ServerChoice = "lifecourse" and year = 1990'
